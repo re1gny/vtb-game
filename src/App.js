0e4666d4804c209f01c54b4afc39d859b9533a7c
dmmanshin <ya.manshin@mail.ru>
2022-12-11 16:37:52 +0300

    add adaptive

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React, { useState } from 'react';
+import React, { useLayoutEffect, useRef, useState } from 'react';
+import useResizeObserver from 'use-resize-observer';
 import { Game } from './components/Game';
 import { GameSelector } from './components/GameSelector';
+import styles from './App.module.scss';
 
 function App() {
   const [department, setDepartment] = useState(null);
+  const [sizeCoefficient, setSizeCoefficient] = useState(1);
 
-  return department ? <Game department={department} /> : <GameSelector onSelectDepartment={setDepartment} />;
+  const appRef = useRef();
+
+  function handleCalculateSizeCoefficient() {
+    setSizeCoefficient((window.innerWidth || 1920) / 1920);
+  }
+
+  useLayoutEffect(() => {
+    handleCalculateSizeCoefficient();
+  }, []);
+
+  useResizeObserver({ onResize: handleCalculateSizeCoefficient, ref: appRef });
+
+  return (
+    <div ref={appRef} className={styles.wrapper} style={{ '--sizeCoefficient': sizeCoefficient }}>
+      {department ? <Game department={department} /> : <GameSelector onSelectDepartment={setDepartment} />}
+    </div>
+  );
 }
 
 export default App;
