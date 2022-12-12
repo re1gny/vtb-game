b8058af3e2f62424070eb4bafbcc9d8378520961
dmmanshin <ya.manshin@mail.ru>
2022-12-12 12:08:35 +0300

    add step duration

--- src/components/BoardBlock/index.js
+++ src/components/BoardBlock/index.js
@@ -5,14 +5,20 @@ import { DEPARTMENT_TITLE } from '../../constants/departments';
 import styles from './index.module.scss';
 
 export function BoardBlock(props) {
-  const { className, department, board, characters, charactersState } = props;
+  const { className, department, board, characters, charactersState, stepDuration } = props;
 
   return (
     <div className={cn(styles.wrapper, className)}>
       <div className={styles.titleWrapper}>
         <div className={styles.title}>{DEPARTMENT_TITLE[department]}</div>
       </div>
-      <Board className={styles.board} board={board} characters={characters} charactersState={charactersState} />
+      <Board
+        className={styles.board}
+        board={board}
+        characters={characters}
+        charactersState={charactersState}
+        stepDuration={stepDuration}
+      />
     </div>
   );
 }
