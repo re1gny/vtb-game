b9f91635ff5df5095f6db7adcf978a59aa179ee5
dmmanshin <ya.manshin@mail.ru>
2022-12-12 13:08:32 +0300

    fix task styles

--- src/components/TaskCardModal/index.js
+++ src/components/TaskCardModal/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { ReactComponent as TaskCardName } from '../../assets/icons/taskCardName.svg';
 import { Modal } from '../Modal';
 import { TaskCardResolver } from '../TaskCardResolver';
 import styles from './index.module.scss';
@@ -7,10 +6,12 @@ import styles from './index.module.scss';
 export function TaskCardModal(props) {
   const { opened, card, onClose } = props;
 
+  const name = ['Задание', card?.number && `№${card.number}`].filter(Boolean).join(' ');
+
   return (
     <Modal className={styles.wrapper} opened={opened} onClose={onClose}>
       <div className={styles.header}>
-        <TaskCardName className={styles.name} />
+        <div className={styles.name}>{name}</div>
       </div>
       <TaskCardResolver className={styles.content} card={card} />
     </Modal>
